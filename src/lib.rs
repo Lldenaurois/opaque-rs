2783ef49228db3dcb9741036b413956f1c22284b
Ladi <Lldenaurois@gmail.com>
2020-04-07 09:07:55 +0200

    Fully functioning roundtrip login

--- src/lib.rs
+++ src/lib.rs
@@ -28,19 +28,23 @@ mod client {
             let xu = Hasher::gen_key();
             let hasher = Hasher::new();
             let alpha = hasher.to_curve(self.password.as_bytes(), r.0.as_ref());
-            let Xu = hasher.exp(&xu)
+            let Xu = hasher.exp(&xu);
             (Session{ r, xu }, alpha, Xu)
         }
 
-        pub fn complete(&self, session: Session, mut beta: secp256k1::PublicKey, Xs: secp256k1::PublicKey, c: Vec<u8>, As: Option<secp256k1::PublicKey>) {
+        pub fn complete(&self, session: Session, mut beta: secp256k1::PublicKey, Xs: secp256k1::PublicKey, c: Vec<u8>, As: Option<secp256k1::PublicKey>) -> Secret {
             // Check that beta is not the generator
             let hasher = Hasher::new();
             let mut buf = [0u8; DIGEST_SIZE];
-            let modulus = gmp::mpz::Mpz::from_str_radix("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16).unwrap();
-            let val = gmp::mpz::Mpz::from_str_radix(&hex::encode(session.r.0.as_ref()), 16).unwrap();
-            let inv = val.invert(&modulus).unwrap();
+            let modulus = gmp::mpz::Mpz::from_str_radix("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16).expect("modulus");
+            let val = gmp::mpz::Mpz::from_str_radix(&hex::encode(session.r.0.as_ref()), 16).expect("val");
+            let inv = val.invert(&modulus).expect("inv");
             let mut buf = [0u8; DIGEST_SIZE];
-            buf.copy_from_slice(&hex::decode(inv.to_str_radix(16)).unwrap());
+            let mut inv_string = inv.to_str_radix(16);
+            if inv_string.len() % 2 != 0 {
+                inv_string = "0".to_string() + &inv_string;
+            }
+            buf.copy_from_slice(&hex::decode(&inv_string).expect("inv to str"));
             let invSecret = Secret(buf);
             hasher.custom_exp(&mut beta, &invSecret);
             let rw = Hasher::hash(self.password.as_bytes(), &beta);
@@ -49,12 +53,31 @@ mod client {
             let aead = Aes256Gcm::new(enc_key);
             let nonce = GenericArray::from_slice(b"unique_nonce");
             let decryption = aead.encrypt(nonce, c.as_ref()).expect("Encryption Failure");
-            let pu = decryption[..DIGEST_SIZE].to_vec();
-            let Pu = decryption[DIGEST_SIZE..DIGEST_SIZE+CURVE_SIZE].to_vec();
-            let Ps = decryption[DIGEST_SIZE+CURVE_SIZE..DIGEST_SIZE+2*CURVE_SIZE].to_vec();
 
-            let Xu = hasher.exp(&xu);
-            let eu = Hasher::hash("Server".as_bytes(), &Xu)
+            let mut pu_buf = [0u8; DIGEST_SIZE];
+            pu_buf.copy_from_slice(&decryption[..DIGEST_SIZE]);
+            let pu = Secret(pu_buf);
+
+            let Pu = secp256k1::PublicKey::from_slice(&decryption[DIGEST_SIZE..DIGEST_SIZE+CURVE_SIZE]).expect("Pu from slice");
+            let Ps = secp256k1::PublicKey::from_slice(&decryption[DIGEST_SIZE+CURVE_SIZE..DIGEST_SIZE+2*CURVE_SIZE]).expect("Ps from slice");
+
+            let Xu = hasher.exp(&session.xu);
+
+            let eu = Secret(Hasher::hash("Server".as_bytes(), &Xu));
+            let es = Secret(Hasher::hash(self.username.as_bytes(), &Xs));
+
+            let mut temp = secp256k1::PublicKey::from_slice(Ps.serialize().as_ref()).expect("temp");
+            hasher.custom_exp(&mut temp, &es);
+            let mut lhs = temp.combine(&Xs).expect("lhs");
+
+            let mut rhs = secp256k1::PublicKey::from_slice(lhs.serialize().as_ref()).expect("rhs");
+            hasher.custom_exp(&mut lhs, &session.xu);
+            hasher.custom_exp(&mut rhs, &eu);
+            hasher.custom_exp(&mut rhs, &pu);
+
+            let out = lhs.combine(&rhs).expect("out");
+
+            Secret(Hasher::hash("".as_bytes(), &out))
         }
 
 
@@ -100,14 +123,11 @@ mod server {
             if self.backend.contains_key(&username_str) {
                 return false;
             }
-            println!("\n\nUsername: {}", username);
             let ks = Hasher::gen_key();
             let rw = self.hasher.oprf(password.as_bytes(), &ks);
             let ps = Hasher::gen_key();
             let Ps = self.hasher.exp(&ps);
             let pu = Hasher::gen_key();
-            println!("pu\t{:?}", pu.0.to_vec());
-            println!("Ps\t{:?}", Ps.serialize().to_vec());
             let Pu = self.hasher.exp(&pu);
             let enc_key = GenericArray::clone_from_slice(rw.as_ref());
             let aead = Aes256Gcm::new(enc_key);
@@ -121,7 +141,7 @@ mod server {
             true
         }
 
-        pub fn process(&self, username: &str, mut alpha: secp256k1::PublicKey) -> Option<(Session, secp256k1::PublicKey, secp256k1::PublicKey, Vec<u8>)> {
+        pub fn process(&self, username: &str, mut alpha: secp256k1::PublicKey, Xu: secp256k1::PublicKey) -> Option<(Secret, Session, secp256k1::PublicKey, secp256k1::PublicKey, Vec<u8>)> {
             // Check that alpha is not generator
             let hasher = Hasher::new();
             let username_str = username.to_string();
@@ -130,7 +150,26 @@ mod server {
                 let session = Session{ xs };
                 hasher.custom_exp(&mut alpha, &file.ks);
                 let Xs = hasher.exp(&session.xs);
-                return Some((session, alpha, Xs, file.c.clone()));
+
+                let es = Secret(Hasher::hash(username.as_bytes(), &Xs));
+                let eu = Secret(Hasher::hash("Server".as_bytes(), &Xu));
+
+                let mut temp = secp256k1::PublicKey::from_slice(file.Pu.serialize().as_ref()).expect("temp");
+                hasher.custom_exp(&mut temp, &eu);
+                let mut lhs = temp.combine(&Xu).expect("lhs");
+
+                let mut rhs = secp256k1::PublicKey::from_slice(lhs.serialize().as_ref()).expect("rhs");
+                hasher.custom_exp(&mut lhs, &session.xs);
+                hasher.custom_exp(&mut rhs, &es);
+                hasher.custom_exp(&mut rhs, &file.ps);
+
+                let out = lhs.combine(&rhs).expect("out");
+
+                let key = Secret(Hasher::hash("".as_bytes(), &out));
+
+                let Ps = hasher.exp(&file.ps);
+
+                return Some((key, session, alpha, Xs, file.c.clone()));
             }
             None
         }
@@ -181,7 +220,7 @@ mod primitives {
             let mut curve_point = Self::sha256_chacha20(input);
             loop {
                 if let Ok(mut point) = secp256k1::PublicKey::from_slice(curve_point.as_ref()) {
-                    point.mul_assign(&self.curve, exp).unwrap();
+                    point.mul_assign(&self.curve, exp).expect("point");
                     return point;
                 }
                 curve_point = Self::sha256_chacha20(curve_point.as_ref());
@@ -199,12 +238,12 @@ mod primitives {
         }
 
         pub fn exp(&self, input:  &Secret) -> secp256k1::PublicKey {
-           let secret = secp256k1::SecretKey::from_slice(input.0.as_ref()).unwrap();
+           let secret = secp256k1::SecretKey::from_slice(input.0.as_ref()).expect("secret");
            secp256k1::PublicKey::from_secret_key(&self.curve, &secret)
         }
 
         pub fn custom_exp(&self, base: &mut secp256k1::PublicKey, input: &Secret) {
-           base.mul_assign(&self.curve, input.0.as_ref()).unwrap();
+           base.mul_assign(&self.curve, input.0.as_ref()).expect("custom_exp");
         }
 
         pub fn oprf(&self, input: &[u8], key: &Secret) -> [u8; DIGEST_SIZE] { 
@@ -216,6 +255,7 @@ mod primitives {
 
 #[cfg(test)]
 mod tests {
+    use sha2::{Sha256, Digest};
     use secp256k1;
     use rand::{self, RngCore};
     use crate::{client, server, primitives};
@@ -226,13 +266,24 @@ mod tests {
     #[test]
     fn it_works() {
         let mut server = server::Server::new();
-        for i in 0..10 {
-            assert!(server.setup(&format!("{}", i), &format!("password{}", i)));
+        let mut bytes = [0u8; DIGEST_SIZE];
+        rand::thread_rng().fill_bytes(&mut bytes);
+        for i in 0..100 {
+            let mut hasher = Sha256::new();
+            hasher.input(bytes.as_ref());
+            hasher.input([i as u8].as_ref());
+            assert!(server.setup(&format!("{}", i), &format!("password{:?}", hasher.result())));
         }
-        let mut client = client::Client{ username: "5".to_string(), password: "password5".to_string() };
-        let (client_session, alpha) = client.login();
-        if let Some((server_session, mut beta, Xs, c)) = server.process(&client.username, alpha) {
-            client.complete(client_session, beta, Xs, c, None);
+        for i in 0..100 {
+            let mut hasher = Sha256::new();
+            hasher.input(bytes.as_ref());
+            hasher.input([ i as u8 ].as_ref());
+            let mut client = client::Client{ username: format!("{}", i), password: format!("password{:?}", hasher.result()) };
+            let (client_session, alpha, Xu) = client.login();
+            if let Some((server_secret, server_session, mut beta, Xs, c)) = server.process(&client.username, alpha, Xu) {
+                let client_secret = client.complete(client_session, beta, Xs, c, None);
+                assert!(server_secret.0.to_vec() == client_secret.0.to_vec());
+            }
         }
     }
 }
